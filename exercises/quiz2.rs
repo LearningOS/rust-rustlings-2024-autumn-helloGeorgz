6c60c25cf146c15e6f509b8d3f4940d7ce60ed88
helloGeorgz <91942275+helloGeorgz@users.noreply.github.com>
2024-10-08 05:44:27 +0000

    使用match匹配，以及string相关的to_owned 以及 repeatapi等

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -17,10 +17,6 @@
 // - The input is going to be a Vector of a 2-length tuple,
 //   the first element is the string, the second one is the command.
 // - The output element is going to be a Vector of strings.
-//
-// No hints this time!
-
-// I AM NOT DONE
 
 pub enum Command {
     Uppercase,
@@ -29,14 +25,23 @@ pub enum Command {
 }
 
 mod my_module {
+    use std::collections::btree_map::Range;
+
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String,Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output:Vec<String> = vec![];
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
+            let handle = match command {
+                Command::Trim => string.trim().to_string(),
+                Command::Uppercase => string.to_uppercase(),
+                Command::Append(n) => string.to_owned()+&"bar".repeat(*n)
+            };
+
+            output.push(handle);
         }
         output
     }
@@ -45,7 +50,7 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
